8fdfdc5b910e2daa27d90ba7f181fcb229bda7fc
elvis878 <elvisf@canapii.com>
2025-11-25 20:43:57 +0800

    updated out dir

--- scripts/restructure-output.js
+++ scripts/restructure-output.js
@@ -2,19 +2,18 @@ const fs = require("fs");
 const path = require("path");
 
 const outDir = path.join(__dirname, "..", "out");
-const pokemonDir = path.join(outDir, "pokemon");
 
 console.log("Restructuring output for clean URLs...");
 
-// Get all .html files in the pokemon directory
-const files = fs.readdirSync(pokemonDir);
+// Get all .html files in the out directory (since routes are now at root level)
+const files = fs.readdirSync(outDir);
 let moved = 0;
 
 files.forEach((file) => {
-  if (file.endsWith(".html") && file !== "index.html") {
+  if (file.endsWith(".html") && file !== "index.html" && file !== "404.html") {
     const name = file.replace(".html", "");
-    const htmlPath = path.join(pokemonDir, file);
-    const folderPath = path.join(pokemonDir, name);
+    const htmlPath = path.join(outDir, file);
+    const folderPath = path.join(outDir, name);
     const newHtmlPath = path.join(folderPath, "index.html");
 
     // Create folder if it doesn't exist
